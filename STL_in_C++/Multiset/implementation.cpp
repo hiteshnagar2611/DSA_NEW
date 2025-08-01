f7db61a4ee969fe54312cc37b10ccc8755061ad6
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2025-08-01 18:01:02 +0000

    .

---
+++ STL_in_C++/Multiset/implementation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    multiset<int> ms;
+    ms.insert(1); //{1}
+    ms.insert(1); //{1,1}
+    ms.insert(1); //{1,1,1}
+
+    ms.erase(1); // all 1's erased
+
+    int cnt = ms.count(1);
+    cout << cnt <<endl;
+    
+    //only a single one erased
+    //ms.erase(ms.find(1));
+    //ms.erase(ms.find(1), ms.find(1) + 2); //{1}
+
+    return 0;
+
+    //rest ALL function are same as set
+
+}
